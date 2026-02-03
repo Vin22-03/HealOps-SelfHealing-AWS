198941f7c25793fbc7ace3539bdd34c8096c1646
Vin22-03 <vinay.venvin@gmail.com>
2026-02-03 09:15:13 +0000

    Updated Jenkinsfile

--- infra/terraform/alb.tf
+++ infra/terraform/alb.tf
@@ -15,7 +15,7 @@ resource "aws_lb" "healops_alb" {
 
 resource "aws_lb_target_group" "healops_tg" {
   name        = "healops-tg"
-  port        = 8080
+  port        = 3000
   protocol    = "HTTP"
   vpc_id      = aws_vpc.healops_vpc.id
   target_type = "ip"
@@ -44,3 +44,4 @@ resource "aws_lb_listener" "healops_listener" {
     target_group_arn = aws_lb_target_group.healops_tg.arn
   }
 }
+ 	
